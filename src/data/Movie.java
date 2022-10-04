432e6c9869e999c37a8b3b67dcae59e1590f201c
lindsaypj <lindsaypj88@gmail.com>
2022-10-04 10:57:35 -0700

    Implemented the data layer and added file with movie data.

--- src/data/Movie.java
+++ src/data/Movie.java
@@ -1,4 +1,56 @@
 package data;
 
+/**
+ * This Movie Object class stores info pertaining to an individual movie.
+ * @author Patrick Lindsay
+ * @version 1.0
+ */
 public class Movie {
+
+    // Fields
+    private String title;
+    private String genre;
+    private String studio;
+    private int audienceScore;
+    private double profitability;
+    private int rtScore;
+    private double worldGross;
+    private int year;
+
+    /**
+     * Constructor for a movie object. Stores given movie data.
+     * @param title of the movie
+     * @param genre of Movie
+     * @param studio where the movie was produced
+     * @param audienceScore of the movie
+     * @param profit how profitable the movie was
+     * @param rtScore Movie's score on Rotten Tomatoes
+     * @param worldGross Worldwide gross profit
+     * @param year movie was released
+     */
+    public Movie(String title, String genre, String studio, int audienceScore, double profit, int rtScore,
+                 double worldGross, int year) {
+        this.title = title;
+        this.genre = genre;
+        this.studio = studio;
+        this.audienceScore = audienceScore;
+        this.profitability = profit;
+        this.rtScore = rtScore;
+        this.worldGross = worldGross;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", studio='" + studio + '\'' +
+                ", audienceScore=" + audienceScore +
+                ", profitability=" + profitability +
+                ", rtScore=" + rtScore +
+                ", worldGross=" + worldGross +
+                ", year=" + year +
+                '}';
+    }
 }
--- src/data/MoviesIO.java
+++ src/data/MoviesIO.java
@@ -1,4 +1,85 @@
 package data;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This class takes a file containing movie data and parses the data into
+ * Movie Objects.
+ * @author Patrick Lindsay
+ * @version 1.0
+ */
 public class MoviesIO {
+
+    // Fields
+    private String filePath;
+    private ArrayList<Movie> movies;
+
+    /**
+     * This is the constructor for the MovieIO object.
+     * Parses data from given file into a list of Movie Objects.
+     * @param filePath path to file with movie data.
+     */
+    public MoviesIO(String filePath) {
+        // Instantiate Fields
+        this.filePath = filePath;
+        this.movies = new ArrayList<>();
+
+        // Try to parse data into movie objects
+        // Store movies in the Movies list
+        try {
+            // Create scanner to parse data
+            Scanner movieScan = new Scanner(new File(filePath));
+
+            // Loop over all records (Entries are separated by line)
+            while (movieScan.hasNextLine()) {
+                // Store next entry
+                String movieEntry = movieScan.nextLine();
+
+                // Parse and store data in new Movie Object
+                movies.add(parse(movieEntry));
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Remove First entry from file (Column headers)
+        if (!movies.isEmpty()) {
+            movies.remove(0);
+        }
+    }
+
+    // Method to parse data in movie entry from file into a Movie Object
+    private static Movie parse(String entry) {
+
+        // Separate data (delimited by commas)
+        String[] data = entry.split(",");
+
+        // Cast raw data from strings to appropriate data-types
+        int audienceScore = Integer.parseInt(data[3]);
+        double profit = Double.parseDouble(data[4]);
+        int rtScore = Integer.parseInt(data[5]);
+        double worldGross = Double.parseDouble(data[6]);
+        int year = Integer.parseInt(data[7]);
+
+        // Film,Genre,Lead Studio,Audience score %,Profitability,Rotten Tomatoes %,Worldwide Gross,Year
+        return new Movie(data[0], data[1], data[2], audienceScore, profit, rtScore, worldGross, year);
+    }
+
+
+    /**
+     * Method to get the list of movies parsed from the file.
+     * @return an ArrayList of Movie Objects
+     */
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    @Override
+    public String toString() {
+        return "MoviesIO: " + filePath;
+    }
 }
